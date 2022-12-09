60816733287c00f6d5641613fd963e77c485b53f
qalifah <oqalifah@gmail.com>
2022-12-09 09:27:31 +0100

    chore: format created_on and updated_on bson representation

--- models.go
+++ models.go
@@ -5,13 +5,13 @@ import "time"
 type Target struct {
 	Id        string     `json:"id" fake:"{uuid}"`
 	Message   string     `json:"message" fake:"{sentence:7}"`
-	CreatedOn time.Time  `json:"created_on"`
-	UpdatedOn *time.Time `json:"updated_on"`
+	CreatedOn time.Time  `json:"created_on" bson:"created_on"`
+	UpdatedOn *time.Time `json:"updated_on" bson:"updated_on"`
 }
 
 type Event struct {
 	Id        string    `json:"id" fake:"{uuid}"`
 	Name      string    `json:"name" fake:"skip"`
 	Targets   []Target  `json:"targets" fakesize:"5"`
-	CreatedOn time.Time `json:"created_on"`
+	CreatedOn time.Time `json:"created_on" bson:"created_on"`
 }
